d418a440b37710788cdbf5adc5f6c7ebb2ac6de8
tristangclvs <goncalvestristan.pro@pm.me>
2022-12-21 15:02:16 +0100

    Tristan : Création des pages about et contact

--- myApp.py
+++ myApp.py
@@ -5,3 +5,12 @@ app = Flask(__name__)
 @app.route("/")
 def index():
 	return render_template('home.html')
+
+
+@app.route("/about")
+def about():
+	return render_template('about.html')
+	
+@app.route("/contact")
+def contact():
+	return render_template('contact.html')
